70bcd7c6762e521ba5e4f0a9a8bc9978445bddc3
Paakofi72 <koo7@njit.edu>
2020-02-01 10:55:47 -0500

    Added division,multiplication,square root.

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -5,3 +5,14 @@ class Calculator:
 
     def addition(self, a, b):
         return a + b
+    def subtraction(self, a, b):
+        return a-b
+    def multiplication(self, a, b):
+        return a*b
+    def division(self, a, b):
+        return a/b
+    def sqaureroot(self,a):
+        import math
+        return (math.sqrt(a))
+    def sqared(self,a):
+        return a**2
